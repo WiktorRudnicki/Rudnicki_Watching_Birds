9cfba581c3d7f9da243eb033cfc3bb6aee5c44e4
WiktorRudnicki <rudnicki.w01@htlwienwest.at>
2022-01-26 08:37:33 +0100

    Image in datatable

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,33 +1,46 @@
 <template>
   <div>
-    <v-data-table :items="birds" :headers="headers"></v-data-table>
+    <v-data-table :headers="headers" :items="birds">
+      <template v-slot:[getImage()]="{item}">
+        <v-img :src="item.pic" style="width: 100px; height: 100px"></v-img>
+        <v-btn @click="addCount(item)"></v-btn>
+      </template>
+    </v-data-table>
   </div>
 </template>
 
 <script>
 import axios from "axios";
 export default {
-  name: 'Home',
-    data: () => ({
-    birds:[],
-    headers:[
+  name: "Home",
+  data: () => ({
+    birds: [],
+    headers: [
       {
-        text: 'id',
-        align: 'start',
+        text: "id",
+        align: "start",
         sortable: true,
-        value: 'id'
+        value: "id",
       },
-      {value: 'v-slot:img'},
-      {text: 'common name', value: 'commonName'},
-      {text: 'scientific name', value: 'scientificName'},
-      {text: 'observed', value: 'count'},
+      { text: "image", value: "pic" },
+      { text: "common name", value: "commonName" },
+      { text: "scientific name", value: "scientificName" },
+      { text: "observed", value: "count" },
     ],
   }),
-  async created(){
-    let response = await axios.get('http://localhost:3000/birds');
+  async created() {
+    let response = await axios.get("http://localhost:3000/birds");
     this.birds = response.data;
     console.log(this.birds);
+  },
+  methods: {
+    getImage(){
+      return `item.pic`;
+    },
+    async addCount(item){
+      let id = item.id;
+      await axios.patch("http://localhost:3000/birds"+id)
+    }
   }
 };
 </script>
-  
